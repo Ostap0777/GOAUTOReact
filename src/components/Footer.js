9c1b5a44ced5e0d18f7e43dd44a89f18fd1f3741
Ostap0777 <kolosivskyyostap@gmail.com>
2024-06-16 16:12:29 +0300

    start header,main and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import icon1 from '../img/icon/iconGoAuto.png';
+import icontelephone from '../img/footer/free-icon-telephone-159832.png'
+
+export default function Footer() {
+  return (
+	 <footer className='footer'>
+	   <div className='footer__container'>
+	     <img className='footer__image' src={icon1} alt='Icon 1' />
+		  <div className='footer__social social'>
+			<a href="#" className='social__link social__facebook'>Go Auto Lviv</a>
+			<a href="#" className='social__link social__instagram'>goautolviv</a>
+			<a href="#" className='social__link social__tiktok'>goautolviv</a>
+			<a href="#" className='social__link social__phone'> 097 430 05 70</a>
+			<a href="#" className='social__link social__telegram'>t.me/goautolviv</a>
+		  </div>
+	  </div>
+	 </footer>
+  )
+}
